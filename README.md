cc6620bd01b9033ab35b83f88c83fa38af4e2fa7
0x2f0 <sarojregmi.official@gmail.com>
2025-12-16 19:03:14 +0545

    chore(auto-gen-readme): corrected generation format.

--- README.md
+++
@@ -1,8 +0,0 @@
-### Here are some of my recent blogs that you can Checkout.
-
----
-
-[USB error -71 in kernel logs (part-1)](https://sarojr.com/blogs/kernel-error-usb-71-part-1)
-
-[Automating 350+ Whatsapp Messages using playwright.](https://sarojr.com/blogs/whatsapp-messages-automation)
-
--- src/lib/utils/gen-readme.ts
+++ src/lib/utils/gen-readme.ts
@@ -4,8 +4,8 @@ import type { TBlog } from '../../routes/blogs/+layout.server';
 const replaceRef = {
 	'{{blog_list}}': (blogs: TBlog[]) => {
 		return blogs.reduce(
-			(p, c) =>
-				`\n[${c.metadata.title.trim().replaceAll('[', '(').replaceAll(']', ')')}](${domain}${c.slug})\n${p}`,
+			(p, c, i) =>
+				`${p}- [${c.metadata.title.trim().replaceAll('[', '(').replaceAll(']', ')')}](${domain}${c.slug})${i !== blogs.length - 1 ? '\n' : ''}`,
 			''
 		);
 	}
@@ -17,15 +17,20 @@ export class genReadMe {
 
 	private templateText = this.template.text();
 	public async writeBlogs(blogs: TBlog[]) {
+		console.log('\n\n[log]: Writing blogs to readme');
+
 		try {
 			let templateText = await this.templateText;
 
 			Object.entries(replaceRef).map(([key, fn]) => {
 				const updatedData = fn(blogs);
+				console.log('[log]: Parsing blog contents to write!!');
+
 				templateText = templateText.replaceAll(key, updatedData);
 			});
 
-			return await this.readMe.write(templateText);
+			await this.readMe.write(templateText);
+			console.log('[log]: Blogs Written Successfully.');
 		} catch (e) {
 			console.log('Something went wrong while writing blogs to readme');
 		}

ce8960a901cbf8fb7c4f5070446ba183915d4b6a
0x2f0 <git@xn--81b3bxa1f.com>
2026-02-12 15:46:55 +0545

    Site Setup

---
+++ src/routes/+error.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import { page } from "$app/state";
+    import { Button } from "$lib/components/ui/button";
+</script>
+
+<svelte:head>
+    <title>Saroj Regmi | Error</title>
+</svelte:head>
+
+<div
+    class="flex flex-col items-center justify-center h-screen w-screen gap-2.5"
+>
+    <h1 class="text-center">Sorry! <br />Looks like this page is broken.</h1>
+    <p>{page.error?.message || "Something went wrong."}</p>
+
+    <Button
+        variant="default"
+        on:click={() => {
+            goto("/");
+        }}>Go home</Button
+    >
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    import Button from "$lib/components/ui/button/button.svelte";
+    import { goto } from "$app/navigation";
+</script>
+
+<svelte:head>
+    <title>Saroj Regmi | Home</title>
+</svelte:head>
+
+<div
+    class="flex flex-col items-center justify-center h-screen w-screen gap-2.5 px-10"
+>
+    <header class="text-center">This portfolio is under construction!</header>
+    <p>But you can check out my blogs.</p>
+
+    <Button
+        on:click={() => {
+            goto("/blog");
+        }}
+        class="mt-10"
+    >
+        Read Blogs
+    </Button>
+</div>
